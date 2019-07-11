f6cde76036e4424d7c748d10b4cd1d531cd383ad
Andrew Short <andrewjshort@gmail.com>
2019-07-11 16:07:02 +1000

    Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import Index from './pages/Index';
+import Login from './pages/Login';
+import Member from './pages/Member';
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Index from './pages/Index';
-import Member from './pages/Member';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Index} />
       <Route path="/member" component={Member} />
+      <Route path="/login" component={Login} />
     </BrowserRouter>
   );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,45 @@
+import Header from '../components/Header';
+import React, { useState } from 'react';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+
+const Index: React.FC = () => {
+  const [number, setNumber] = useState(0);
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  return (
+    <>
+      <Header />
+      <Container className="login my-3">
+        <Form>
+          <FormGroup>
+            <Label for="number">Member number</Label>
+            <Input
+              type="number"
+              name="number"
+              id="number"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup check className="mb-3">
+            <Label check>
+              <Input type="checkbox" name="remember" /> Remember me?
+            </Label>
+          </FormGroup>
+          <Button type="submit" color="primary" size="lg" block>Log In</Button>
+        </Form>
+      </Container>
+    </>
+  );
+};
+
+export default Index;
