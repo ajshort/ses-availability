03bd92f65a85e86328c14241878aa7c0a776e796
Andrew Short <andrewjshort@gmail.com>
2019-07-12 13:04:17 +1000

    Member availability UI

--- src/pages/Member.tsx
+++ src/pages/Member.tsx
@@ -1,6 +1,8 @@
+import { Availability, RescueAvailable, StormAvailable } from '../types/availability';
+import { Shift, defaultShifts } from '../types/shifts';
 import moment, { Moment } from 'moment';
 import React, { useState } from 'react';
-import { FaMinus, FaPlus } from 'react-icons/fa';
+import { FaBolt, FaCarSide, FaExclamationTriangle, FaInfo, FaMinus } from 'react-icons/fa';
 import {
   Badge,
   Button,
@@ -14,15 +16,21 @@ import {
   Modal,
   ModalBody,
   ModalFooter,
-  ModalHeader,
   ModalProps,
   Row,
   Table,
+  BadgeProps,
 } from 'reactstrap';
 
+function getShiftLabel(shift: Shift): string {
+  const from = moment().set(shift.from);
+  const to = moment().set(shift.to);
+  return `${from.format('HHmm')} - ${to.format('HHmm')}`;
+}
+
 interface ShiftsTableProps {
-  days: Array<{ date: Moment }>;
-  shifts: Array<{ label: string; }>;
+  days: Array<Moment>;
+  shifts: Array<Shift>;
 }
 
 const ShiftsTable: React.FC<ShiftsTableProps> = ({ days, shifts }) => (
@@ -30,19 +38,19 @@ const ShiftsTable: React.FC<ShiftsTableProps> = ({ days, shifts }) => (
     <thead>
       <tr>
         <th />
-        {days.map(({ date }) => (
-          <th key={date.unix()}>
-            {date.format('ddd D/M')}
+        {days.map((day) => (
+          <th key={day.unix()}>
+            {day.format('ddd D/M')}
           </th>
         ))}
       </tr>
     </thead>
     <tbody>
       {shifts.map(shift => (
-        <tr key={shift.label}>
-          <th>{shift.label}</th>
-          {days.map(({ date }) => (
-            <th key={date.unix()}>
+        <tr key={shift.key}>
+          <th>{getShiftLabel(shift)}</th>
+          {days.map(day => (
+            <th key={day.unix()}>
               <Label check>
                 <input type="checkbox" />
               </Label>
@@ -78,13 +86,10 @@ const TimesTable: React.FC = () => (
   </Table>
 );
 
-type StormAvailable = 'available' | 'unavailable' | undefined;
-type RescueAvailable = 'immediate' | 'support' | 'unavailable' | undefined;
-
 const EditModal: React.FC<ModalProps> = (props) => {
   const [ui, setUI] = useState<'shifts' | 'times' | 'week'>('shifts');
-  const [storm, setStorm] =  useState<StormAvailable>(undefined);
-  const [rescue, setRescue] = useState<RescueAvailable>(undefined);
+  const [storm, setStorm] =  useState<StormAvailable | undefined>(undefined);
+  const [rescue, setRescue] = useState<RescueAvailable | undefined>(undefined);
   const [note, setNote] = useState('');
 
   return (
@@ -215,12 +220,127 @@ const EditModal: React.FC<ModalProps> = (props) => {
   );
 };
 
-interface Props {
-  days: Array<{ date: Moment; }>;
+interface StormBadgeProps extends BadgeProps {
+  available?: StormAvailable;
 }
 
-const MemberAvailabilityTable: React.FC<Props> = ({ days }) => (
-  null
+const StormBadge: React.FC<StormBadgeProps> = ({ available, ...props }) => {
+  if (available === 'available') {
+    return <Badge color="success" {...props}><FaBolt /></Badge>;
+  }
+  if (available === 'unavailable') {
+    return <Badge color="danger" {...props}><FaBolt /></Badge>;
+  }
+
+  return null;
+};
+
+interface RescueBadgeProps extends BadgeProps {
+  available?: RescueAvailable;
+}
+
+const RescueBadge: React.FC<RescueBadgeProps> = ({ available, ...props }) => {
+  if (available === 'immediate') {
+    return <Badge color="success" {...props}><FaExclamationTriangle /></Badge>;
+  }
+  if (available === 'support') {
+    return <Badge color="warning" {...props}><FaExclamationTriangle /></Badge>;
+  }
+  if (available === 'unavailable') {
+    return <Badge color="danger" {...props}><FaExclamationTriangle /></Badge>;
+  }
+
+  return null;
+};
+
+interface MemberAvailabilityTableProps {
+  availabilities: Array<Availability>;
+  days: Array<Moment>;
+  shifts: Array<Shift>;
+}
+
+const MemberAvailabilityTable: React.FC<MemberAvailabilityTableProps> = ({ availabilities, days, shifts }) => (
+  <Table className="member-availability-table">
+    <thead>
+      <tr>
+        <th>Date</th>
+        {shifts.map((shift) => (
+          <th key={shift.key} className="shift">{getShiftLabel(shift)}</th>
+        ))}
+      </tr>
+    </thead>
+    <tfoot>
+      <tr>
+        <th colSpan={shifts.length + 1}>
+          <Button color="primary" className="mr-2">Edit Availability</Button>
+          <Button color="secondary">Copy from Previous Week</Button>
+        </th>
+      </tr>
+    </tfoot>
+    <tbody>
+      {days.map(day => {
+        // Get all shifts which fall within this shift day.
+        const from = day.clone().set(shifts[0].from);
+        const to = day.clone().set(shifts[shifts.length - 1].to);
+        const duration = to.unix() - from.unix();
+
+        const entries = availabilities.filter(entry => (
+          entry.from.isBefore(to) && entry.to.isSameOrAfter(from)
+        ));
+
+        return (
+          <tr key={day.unix()}>
+            <td>{day.format('ddd D/M')}</td>
+            <td colSpan={shifts.length} style={{ position: 'relative' }}>
+              {entries.map(entry => {
+                const dayFrom = Math.max(entry.from.unix(), from.unix());
+                const dayTo = Math.min(entry.to.unix(), to.unix());
+
+                const left = (100 * (dayFrom - from.unix()) / duration).toString() + '%';
+                const right = (100 - 100 * (dayTo - from.unix()) / duration).toString() + '%';
+
+                let color;
+
+                if (entry.storm === 'available' && entry.rescue === 'immediate') {
+                  color = 'success';
+                } else {
+                  color = 'danger';
+                }
+
+                return (
+                  <div
+                    className={`alert alert-${color} d-flex align-items-center justify-content-center`}
+                    style={{
+                      left,
+                      right,
+                      bottom: '0.5rem',
+                      top: '0.5rem',
+                      margin: 0,
+                      position: 'absolute',
+                    }}
+                  >
+                    <StormBadge available={entry.storm} className="mr-1" />
+                    <RescueBadge available={entry.rescue} className="mr-1" />
+                    {entry.vehicle && (
+                      <Badge color="info" className="mr-1">
+                        <FaCarSide /> <span className="d-none d-md-inline">{entry.vehicle}</span>
+                      </Badge>
+                    )}
+                    {entry.note && (
+                      <Badge color="secondary">
+                        <FaInfo className="d-md-none" />
+                        <span className="d-none d-md-inline">{entry.note}</span>
+                      </Badge>
+                    )}
+                  </div>
+                );
+              })}
+            </td>
+          </tr>
+        );
+      })}
+    </tbody>
+  </Table>
 );
 
 const Member: React.FC = () => {
@@ -228,43 +348,49 @@ const Member: React.FC = () => {
   const toggleEditing = () => setEditing(!isEditing);
 
   const days = [
-    { date: moment('2019-07-01') },
-    { date: moment('2019-07-02') },
-    { date: moment('2019-07-03') },
-    { date: moment('2019-07-04') },
-    { date: moment('2019-07-05') },
-    { date: moment('2019-07-06') },
-    { date: moment('2019-07-07') },
-    { date: moment('2019-07-08') },
+    moment('2019-07-01'),
+    moment('2019-07-02'),
+    moment('2019-07-03'),
+    moment('2019-07-04'),
+    moment('2019-07-05'),
+    moment('2019-07-06'),
+    moment('2019-07-07'),
+    moment('2019-07-08'),
   ];
 
-  const shifts = [
-    { label: '0600 - 1200' },
-    { label: '1200 - 1800' },
-    { label: '1800 - 0600' },
+  const availabilities = [
+    {
+      from: moment('2019-07-02 13:00'),
+      to: moment('2019-07-05 15:00'),
+      storm: 'available' as StormAvailable,
+      rescue: 'immediate' as RescueAvailable,
+      vehicle: 'WOL43',
+    },
+    {
+      from: moment('2019-07-05 15:00'),
+      to: moment('2019-07-10 15:00'),
+      storm: 'unavailable' as StormAvailable,
+      rescue: 'unavailable' as RescueAvailable,
+      note: 'OOA',
+    },
   ];
 
   return (
     <>
       <Container className="my-3">
-        <div className="d-md-flex flex-md-row justify-content-md-between">
-          <div>
-            <h1>Member Name</h1>
-            <p className="lead">Week of 1st July 2019</p>
-          </div>
-          <div className="mb-1">
-            <Button color="primary" onClick={toggleEditing}>Edit Availability</Button>
-          </div>
-        </div>
+        <h1>Member Name</h1>
+        <p className="lead">Week of 1st July 2019</p>
         <MemberAvailabilityTable
           days={days}
+          shifts={defaultShifts}
+          availabilities={availabilities}
         />
       </Container>
       <EditModal
         isOpen={isEditing}
         toggle={toggleEditing}
         days={days}
-        shifts={shifts}
+        shifts={defaultShifts}
       />
     </>
   );
