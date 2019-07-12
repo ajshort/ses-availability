ceb866d64738acc71c0abbfffd649546692b0eb9
Andrew Short <andrewjshort@gmail.com>
2019-07-12 13:44:49 +1000

    Route titles

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,30 @@ import Header from './components/Header';
 import Index from './pages/Index';
 import Login from './pages/Login';
 import Member from './pages/Member';
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Switch, RouteProps } from 'react-router-dom';
+
+interface AppRouteProps extends RouteProps {
+  title: string;
+  isProtected?: boolean;
+}
+
+const AppRoute: React.FC<AppRouteProps> = ({ title, isProtected, ...props }) => {
+  useEffect(() => {
+    document.title = `${title} | SES Availability`;
+  });
+
+  return <Route {...props} />;
+};
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Header />
       <Switch>
-        <Route path="/" exact component={Index} />
-        <Route path="/member" component={Member} />
-        <Route path="/login" component={Login} />
+        <AppRoute path="/" title="Home" exact isProtected component={Index} />
+        <AppRoute path="/member" title="Member" isProtected component={Member} />
+        <AppRoute path="/login" title="Login" component={Login} />
       </Switch>
     </BrowserRouter>
   );
